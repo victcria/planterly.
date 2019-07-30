21f57bf98cdc2bb562cc51375fef261513eb5338
Victoria <victoria.mai@kippbastudent.org>
2019-07-30 10:17:59 -0700

    added a bunch

---
+++ planterly./ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  planterly.
+//
+//  Created by Workday on 7/30/19.
+//  Copyright © 2019 cool kids. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
